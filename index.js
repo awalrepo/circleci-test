ae9b32f64bcabcc8b8dc68d209494ac836261888
awalrepo <awalletter@gmail.com>
2024-04-19 01:53:29 +0700

    tist

--- index.js
+++ index.js
@@ -16,18 +16,48 @@ async function sendSlackMessage ({blocks, slackBotToken, slackChannelId}) {
     return slackClient.chat.postMessage({
         blocks: blocks,
         channel: slackChannelId,
-        unfurl_links: false,
+        text: '-'
+    });
+};
+
+async function updateSlackMessage ({blocks, slackBotToken, slackChannelId, ts}) {
+    if (!slackBotToken) {
+        console.error('Failed to post on Slack Channel: slackBotToken not provided');
+        return;
+    }
+
+    if (!slackChannelId) {
+        console.error('Failed to post on Slack Channel: slackChannelId not provided');
+        return;
+    }
+
+    const slackClient = new WebClient(slackBotToken);
+
+    return slackClient.chat.postMessage({
+        blocks: blocks,
+        channel: slackChannelId,
+        text: '-',
+        ts: ts
     });
 };
 
 async function main() {
     try {
-        const response = await sendSlackMessage({
-            slackBotToken: process.env.SLACK_ACCESS_TOKEN,
-            slackChannelId: 'C06V5CYCK32',
-            blocks: process.env.FBZ_TEMPLATE_START,
-        });
-        console.log(`export SLACK_MESSAGE_TS=${response.ts}`)
+        if (process.env.SLACK_MESSAGE_TS) {
+            await updateSlackMessage({
+                slackBotToken: process.env.SLACK_ACCESS_TOKEN,
+                slackChannelId: 'C06V5CYCK32',
+                blocks: process.env.FBZ_TEMPLATE_START,
+                ts: process.env.SLACK_MESSAGE_TS
+            });
+        } else {
+            const response = await sendSlackMessage({
+                slackBotToken: process.env.SLACK_ACCESS_TOKEN,
+                slackChannelId: 'C06V5CYCK32',
+                blocks: process.env.FBZ_TEMPLATE_START,
+            });
+            console.log(`export SLACK_MESSAGE_TS=${response.ts}`)
+        }
     } catch (error) {
         console.error('Error running smoke tests:', error);
     }
