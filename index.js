f0b7b918c222a7650649f8adf88ef2005889715a
awalrepo <awalletter@gmail.com>
2024-04-19 02:30:15 +0700

    debug

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ async function updateSlackMessage ({blocks, slackBotToken, slackChannelId, ts}) 
 async function main() {
     try {
         if (process.env.SLACK_MESSAGE_TS) {
+            console.log(`masuk sini: ${process.env.SLACK_MESSAGE_TS}`, process.env.FBZ_TEMPLATE_START);
             await updateSlackMessage({
                 slackBotToken: process.env.SLACK_ACCESS_TOKEN,
                 slackChannelId: 'C06V5CYCK32',
