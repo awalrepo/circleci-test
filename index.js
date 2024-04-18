485f6c3f97c394b4d4972a4acb23b79d2c8ba51d
awalrepo <awalletter@gmail.com>
2024-04-18 22:34:40 +0700

    test

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-import { sendSlackMessage } from './utils';
+import { WebClient } from '@slack/web-api';
+
+async function sendSlackMessage (blocks, slackBotToken, slackChannelId) {
+    if (!slackBotToken) {
+        console.error('Failed to post on Slack Channel: slackBotToken not provided');
+        return;
+    }
+
+    if (!slackChannelId) {
+        console.error('Failed to post on Slack Channel: slackChannelId not provided');
+        return;
+    }
+
+    const slackClient = new WebClient(slackBotToken);
+
+    return slackClient.chat.postMessage({
+        blocks,
+        channel: slackChannelId,
+        unfurl_links: false,
+    });
+};
 
 async function main() {
     try {
-        const response = sendSlackMessage({
-            slackBotToken: process.env.SLACK_ACCESS_TOKEN,
-            slackChannelId: 'C06V5CYCK32',
-            message: process.env.FBZ_TEMPLATE_START,
-        });
-        console.log(`ts: ${response.ts}`)
+        // const response = await sendSlackMessage({
+        //     slackBotToken: process.env.SLACK_ACCESS_TOKEN,
+        //     slackChannelId: 'C06V5CYCK32',
+        //     message: process.env.FBZ_TEMPLATE_START,
+        // });
+        console.log('yellow')
     } catch (error) {
         console.error('Error running smoke tests:', error);
     }
